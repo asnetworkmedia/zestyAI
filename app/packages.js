b117e0c8920e64319678904506d79191abefe47a
HamidShahrokhi <hamid.shahrokhi@limolin.com>
2026-01-12 11:28:49 -0500

    refactor: remove unused packages and clean up package.json files

--- app/packages.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  runtime: ['vue', 'vue-router', 'axios'],
-  styling: ['tailwindcss', 'autoprefixer', 'postcss'],
-  build: ['@vue/cli-service', '@vue/cli-plugin-babel', '@vue/compiler-sfc']
-};
