533aed092efd20cf9e531f64129ea21110328c05
HamidShahrokhi <hamid.shahrokhi@limolin.com>
2026-01-12 17:29:13 -0500

    docs: improve comments for geolocation fields in Property model for clarity

--- api/src/models/property.js
+++ api/src/models/property.js
@@ -9,23 +9,23 @@ const Property = sequelize.define(
       type: DataTypes.STRING(100),
       primaryKey: true
     },
-    geocode_geo: {
+    geocode_geo: { // Point - the primary point location for the property in WGS84 (SRID 4326), stored as a lon/lat
       type: DataTypes.GEOGRAPHY('POINT', 4326),
       allowNull: true
     },
-    parcel_geo: {
+    parcel_geo: { // Polygon - the parcel boundary polygon in WGS84 (SRID 4326)
       type: DataTypes.GEOGRAPHY('POLYGON', 4326),
       allowNull: true
     },
-    building_geo: {
+    building_geo: { // Polygon - the building footprint polygon in WGS84 (SRID 4326)
       type: DataTypes.GEOGRAPHY('POLYGON', 4326),
       allowNull: true
     },
-    image_bounds: {
+    image_bounds: { // Array of 4 doubles [minLongitude, minLatitude, maxLongitude, maxLatitude] or so called [west, south, east, north]
       type: DataTypes.ARRAY(DataTypes.DOUBLE),
       allowNull: true
     },
-    image_url: {
+    image_url: { // URL string
       type: DataTypes.TEXT,
       allowNull: true
     }
